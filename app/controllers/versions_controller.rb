0131d7260ad2a552c80346bddc7633246ad9efac
djtal <guillaume.garcera@gmail.com>
2011-10-06 22:18:14 +0200

    update version workflow UI

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -39,6 +39,7 @@ class VersionsController < ApplicationController
   # GET /versions/1/edit
   def edit
     @version = Version.find(params[:id])
+    @project = Project.find(params[:project_id])
   end
 
   # POST /versions
@@ -50,7 +51,7 @@ class VersionsController < ApplicationController
 
     respond_to do |format|
       if @version.save
-        format.html { redirect_to @project, notice: 'Version was successfully created.' }
+        format.html { redirect_to [@project,@version], notice: 'Version was successfully created.' }
         format.json { render json: @version, status: :created, location: @version }
       else
         format.html { render action: "new" }
@@ -62,11 +63,12 @@ class VersionsController < ApplicationController
   # PUT /versions/1
   # PUT /versions/1.json
   def update
+    @project = Project.find(params[:project_id])
     @version = Version.find(params[:id])
 
     respond_to do |format|
       if @version.update_attributes(params[:version])
-        format.html { redirect_to @version, notice: 'Version was successfully updated.' }
+        format.html { redirect_to [@project,@version], notice: 'Version was successfully updated.' }
         format.json { head :ok }
       else
         format.html { render action: "edit" }
@@ -78,11 +80,12 @@ class VersionsController < ApplicationController
   # DELETE /versions/1
   # DELETE /versions/1.json
   def destroy
+    @project = Project.find(params[:project_id])
     @version = Version.find(params[:id])
     @version.destroy
 
     respond_to do |format|
-      format.html { redirect_to versions_url }
+      format.html { redirect_to @project }
       format.json { head :ok }
     end
   end
